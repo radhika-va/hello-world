7768eb3dbf3adb5d4ce5d4d083fb6f8a618d616a
radhia-va <radhika.kumar@va.gov>
2019-09-16 10:00:50 -0400

    Create hello-world-code.cpp
    code modified to displat hello world based on user input

---
+++ code/hello-world-code.cpp
@@ -0,0 +1,19 @@
+//c++ code to display "hello world"
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+string input;
+//Enter user input
+cout << "Enter your input as YES if you want Hello world to be displayed: ";
+cin >> input;
+//print hello world if input is YES
+
+if(input == YES)
+{
+cout <<"Hello World";
+}
+return 0;
+}
